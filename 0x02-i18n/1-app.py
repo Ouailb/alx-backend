4b009efe4c96462ed329b448723f55867975d438
ouailb <adomeyou@gmail.com>
2024-07-08 11:30:04 +0100

    ++

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" app """
+
+
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template
+
+
+class Config(object):
+    """ config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """ home page """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
